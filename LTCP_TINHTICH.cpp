b48b958705ee62d86d1f1f44a8ba9ce6923d7ad3
ADMIN <ADMIN@LAB1-MAY33>
2024-02-26 08:38:46 +0700

    Add project files.

---
+++ LTCP_TINHTICH.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tich;
+	cout << "CHUONG TRINH TINH TICH 2 SO\n";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "Nhap so thu hai:";
+	cin >> so2;
+	tich = so1 * so2;
+	cout << so1 << " * " << so2 << " = " << tich << endl;
+	return 0;
+}
